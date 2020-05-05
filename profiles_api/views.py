c851889d49ee8d6ae5c0d918bc0c1bbc6b413bd6
prem-techchefs <prem.techchef@razorpay.com>
2020-05-05 17:13:22 +0530

    Added post, put, patch, delete method to api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
 	"""Test Api View"""
+	serializer_class = serializers.HelloSerializer
 
 	def get(self, request, format=None):
 		"""Returns a list of APIViews features"""
@@ -15,3 +19,52 @@ class HelloApiView(APIView):
 		]
 
 		return Response({'message':'Hello!', 'an_apiview': an_apiview})
+
+
+	def post(self, request):
+		"""Create a hello message with our name"""
+		serializer = self.serializer_class(data=request.data)
+
+		if serializer.is_valid():
+			name = serializer.validated_data.get("name")
+			message = f'Hello {name}'
+
+			return Response({'message':message})
+		else:
+			return Response(
+				serializer.errors,
+				status = status.HTTP_400_BAD_REQUEST
+			)
+
+
+	def put(self, request, pk=None):
+		"""Handle updating object"""
+		return Response({'methon':'PUT'})
+
+
+	def patch(self, request, pk=None):
+		"""Handle a partial update of an object"""
+		return Response({'mehtod':'PATCH'})
+
+
+	def delete(self, request, pk=None):
+		"""Delete an object"""
+		return Response({'mehtod':'DELETE'})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
